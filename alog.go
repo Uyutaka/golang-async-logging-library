97284e5212432e34ead87b5ee11f8ba573a3cf13
uyutaka <yutaka.urakami@colorado.edu>
2023-03-27 03:34:18 +0000

    Writing Asynchronous Log Entries | 7

--- alog.go
+++ alog.go
@@ -41,7 +41,10 @@ func New(w io.Writer) *Alog {
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-
+	for {
+		msg := <-al.msgCh
+		go al.write(msg, nil)
+	}
 }
 
 func (al Alog) formatMessage(msg string) string {
